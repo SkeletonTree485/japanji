88d1d37af1b650d7ace0ad105603c760a83867a8
SkeletonTree485 <136203447+SkeletonTree485@users.noreply.github.com>
2025-12-14 12:32:18 +0100

    fixed missing commas

--- questions.js
+++ questions.js
@@ -1,20 +1,20 @@
 const quizQuestions = [
   {
     question: "Should they accept the gift from the sumo master?",
-    answers: ["Yes", "No"],
-    correctIndex: 1
+    answers: ["Yes","No"],
+    correctIndex: 1,
     background: "linear-gradient(135deg, #f5af19, #f12711)"
   },
   {
     question: "Should they take off their clothes?",
     answers: ["No", "Yes"],
-    correctIndex: 1
+    correctIndex: 1,
     background: "linear-gradient(135deg, #1f1e1cff, #f12711)"
   },
   {
     question: "Should she continue singing?",
     answers: ["Yes","No"],
-    correctIndex: 1
+    correctIndex: 1,
     background: "linear-gradient(135deg, #f5af19, #f12711)"
   }
 ];
