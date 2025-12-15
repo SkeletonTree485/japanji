954a5c8214e497760db22d490418c11ead6faa40
SkeletonTree485 <136203447+SkeletonTree485@users.noreply.github.com>
2025-12-15 23:50:58 +0100

    cat emojis

--- questions.js
+++ questions.js
@@ -1,19 +1,19 @@
 const quizQuestions = [
   {
     question: "Should they accept the gift from the sumo master?",
-    answers: ["Nice! Gimme free stuff!","Politely decline the gift."],
+    answers: ["Nice! Gimme free stuff! ฅ^•ﻌ•^ฅ","Politely decline the gift. ^-^"],
     correctIndex: 1,
     backgroundClass: "theme-sumo"
   },
   {
     question: "Should they take off their clothes?",
-    answers: ["Absolutely not, this is a family-friendly game!", "Absolutely, yes! I wanna see naked ppl!"],
+    answers: ["Absolutely not, this is a family-friendly game! =^.^=", "Absolutely, yes! I wanna see naked ppl! (=^･ω･^=)"],
     correctIndex: 1,
     backgroundClass: "theme-bath"
   },
   {
     question: "Should she continue singing?",
-    answers: ["Wow! I like her beautiful voice","Who the fuck let her sing?"],
+    answers: ["Wow! I like her beautiful voice! ⚞ • ⚟","Who the fuck let her sing? (•- •マ"],
     correctIndex: 1,
     backgroundClass: "theme-karaoke"
   }
